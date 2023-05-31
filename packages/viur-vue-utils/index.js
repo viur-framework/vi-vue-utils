540ec0a70217e7315b7bc09ff0958cb0aa53b0d4
XeoN-GHMB <ak@mausbrand.de>
2023-05-31 14:03:29 +0200

    fix: export boneLogics

--- packages/viur-vue-utils/index.js
+++ packages/viur-vue-utils/index.js
@@ -2,6 +2,7 @@ import {Request, HTTPError} from './utils/request'
 import {ListRequest, destroyStore} from './utils/handlers'
 import bone from './bones/edit/bone.vue'
 import {getBoneWidget, useBoneStore, addBoneWidget} from './bones/edit/index'
+import boneLogic from './bones/view/boneLogic'
 import de_translations from './translations/de';
 import en_translations from './translations/en';
 
@@ -15,5 +16,6 @@ export {
   useBoneStore,
   addBoneWidget,
   de_translations,
-  en_translations
+  en_translations,
+  boneLogic
 }
