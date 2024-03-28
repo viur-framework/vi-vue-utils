bdeb6cc34a41bbbccd436d1b31cdfbde6b4826f4
phneutral <chris@ph-neutral.net>
2024-03-28 12:34:30 +0100

    feat: custom property hooks for bone-label

--- packages/viur-vue-utils/bones/edit/boneLabel.vue
+++ packages/viur-vue-utils/bones/edit/boneLabel.vue
@@ -47,26 +47,37 @@ export default defineComponent({
 </script>
 
 <style scoped>
+
 .bone-name {
+  --font-size: var(--sl-input-font-size-medium);
+  --height: var(--sl-input-height-medium);
+  --spacing-inline: var(--sl-input-spacing-medium);
+  --color: var(--sl-color-neutral-900);
+  --background: var(--sl-color-neutral-200);
+  --border-width: 0;
+  --border-style: none;
+  --border-color: transparent;
+  --border-radius: var(--sl-input-border-radius-medium);
   display: flex;
   flex-direction: row;
   align-items: center;
   align-self: flex-start;
-  font-size: var(--sl-input-font-size-medium);
-  min-height: var(--sl-input-height-medium);
-  padding: 0.4em 0.7em;
-  color: var(--sl-color-neutral-900);
-  background-color: var(--sl-color-neutral-200);
-  border: none;
-  border-top-left-radius: var(--sl-input-border-radius-medium);
-  border-bottom-left-radius: var(--sl-input-border-radius-medium);
+  font-size: var(--font-size);
+  min-height: var(--height);
+  line-height: calc(var(--height) - 2 * var(--border-width));
+  padding-inline: var(--spacing-inline);
+  color: var(--color);
+  background: var(--background);
+  border-width: var(--border-width);
+  border-style: var(--border-style);
+  border-color: var(--border-color);
+  border-radius: var(--border-radius) 0 0 var(--border-radius);
   word-break: break-word;
-
   @media (max-width: 900px) {
-    border-top-right-radius: var(--sl-input-border-radius-medium);
-    border-bottom-left-radius: 0;
+    --border-radius: var(--border-radius) var(--border-radius) 0 0;
   }
 }
+
 .debug {
   display: flex;
   align-items: center;
