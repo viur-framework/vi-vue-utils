a59b94a4b85093c1368950fc86f0b50035ff5cf4
KadirBalku <Kadir.Balku@gmail.com>
2024-04-15 15:23:42 +0200

    feat: Render raw.json in codeblock with highlighting and auto code formatting

--- packages/viur-vue-utils/bones/edit/default/rawBone.vue
+++ packages/viur-vue-utils/bones/edit/default/rawBone.vue
@@ -1,9 +1,14 @@
 <template>
-  <sl-textarea v-if="boneState.bonestructure.type==='raw.json'"
-    :disabled="boneState?.readonly"
-    :value="JSON.stringify(value)"
+  <prism-editor
+    v-if="boneState.bonestructure.type === 'raw.json'"
+    v-model="state.value"
+    class="my-editor"
+    :highlight="highlighter"
+    :readonly="boneState.bonestructure.readonly"
+    line-numbers
     @input="changeEvent"
-  ></sl-textarea>
+  >
+  </prism-editor>
   <sl-textarea
     v-else
     :disabled="boneState?.readonly"
@@ -15,6 +20,18 @@
 <script lang="ts">
 //@ts-nocheck
 import { reactive, defineComponent, onMounted, inject } from "vue"
+import { PrismEditor } from "vue-prism-editor"
+import "vue-prism-editor/dist/prismeditor.min.css" // import the styles somewhere
+
+// import highlighting library (you can use any library you want just return html string)
+import { highlight, languages } from "prismjs/components/prism-core"
+import "prismjs/components/prism-clike"
+import "prismjs/components/prism-javascript"
+import "prismjs/themes/prism-tomorrow.css" // import syntax highlighting styles
+
+import * as prettier from "prettier"
+import pluginEstree from "prettier/plugins/estree"
+import pluginBabel from "prettier/plugins/babel"
 
 export default defineComponent({
   inheritAttrs: false,
@@ -24,31 +41,72 @@ export default defineComponent({
     index: Number,
     lang: String
   },
-  components: {},
+  components: { PrismEditor },
   emits: ["change"],
   setup(props, context) {
     const boneState = inject("boneState")
     const state = reactive({
+      value: ""
     })
 
+    //TODO: Save doesnt work because too many fields are sent (Bad Request Error)
     function changeEvent(event) {
       context.emit("change", props.name, event.target.value, props.lang, props.index)
     }
 
-    onMounted(() => {
+    function highlighter(code) {
+      return highlight(code, languages.js) // languages.<insert language> to return html with markup
+    }
+
+    async function formatCode(code) {
+      if (code !== "null" || code !== "undefined") {
+        code = await prettier.format(code, { semi: false, parser: "json", plugins: [pluginEstree, pluginBabel] })
+      }
+
+      return code
+    }
+
+    onMounted(async () => {
+      state.value = await formatCode(JSON.stringify(props.value))
+
       context.emit("change", props.name, props.value, props.lang, props.index) //init
     })
 
     return {
       state,
       boneState,
-      changeEvent
+      changeEvent,
+      highlighter
     }
   }
 })
 </script>
 
 <style scoped>
+/* required class */
+.my-editor {
+  /* we dont use `language-` classes anymore so thats why we need to add background and text color manually */
+  background: #2d2d2d;
+  color: #ccc;
+
+  /* you must provide font-family font-size line-height. Example: */
+  font-family:
+    Fira code,
+    Fira Mono,
+    Consolas,
+    Menlo,
+    Courier,
+    monospace;
+  font-size: 14px;
+  line-height: 1.5;
+  padding: 5px;
+}
+
+/* optional class for removing the outline */
+.prism-editor__textarea:focus {
+  outline: none;
+}
+
 sl-input {
   width: 100%;
 
